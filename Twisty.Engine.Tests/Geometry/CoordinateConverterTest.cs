f4add4cf1ba18dbe041e03c61e8f86472ad63726
jardons <sebastien.jardon@moodys.com>
2019-11-29 21:12:49 +0100

    Create HomogeneousCoordinate
    Create HomogeneousCoordinate to provide additional tools for further vector manipulations.

--- Twisty.Engine.Tests/Geometry/CoordinateConverterTest.cs
+++ Twisty.Engine.Tests/Geometry/CoordinateConverterTest.cs
@@ -11,6 +11,7 @@ namespace Twisty.Engine.Tests.Geometry
 
 		#region Test Data
 
+		//(double phi, double theta, double x, double y, double z)
 		public static readonly TheoryData<double, double, double, double, double> SphericalAndCartesian = new TheoryData<double, double, double, double, double>()
 		{
 			{0.0, 0.0, 0.0, 0.0, 1.0},
@@ -21,13 +22,29 @@ namespace Twisty.Engine.Tests.Geometry
 			{Math.PI / 4.0 * 3.0, Math.PI / 4.0, -0.5, 0.5, 0.70710678118654757 },
 		};
 
+		//(double hx, double hy, double hz, double hw, double x, double y, double z)
+		public static readonly TheoryData<double, double, double, double, double, double, double> HomogeneousAndCartesian = new TheoryData<double, double, double, double, double, double, double>()
+		{
+			{1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0},
+			{1.0, 2.0, 3.0, 1.0, 1.0, 2.0, 3.0},
+			{0.0, 0.0, 9.0, 1.0, 0.0, 0.0, 9.0},
+		};
+
+		//(double hx, double hy, double hz, double hw, double x, double y, double z)
+		public static readonly TheoryData<double, double, double, double, double, double, double> HomogeneousToCartesianOnly = new TheoryData<double, double, double, double, double, double, double>()
+		{
+			{2.0, 0.0, 0.0, 2.0, 1.0, 0.0, 0.0},
+			{4.0, 8.0, 12.0, 4.0, 1.0, 2.0, 3.0},
+			{0.0, 0.0, 4.5, 0.5, 0.0, 0.0, 9.0},
+		};
+
 		#endregion Test Data
 
 		#region Test Methods
 
 		[Theory]
 		[MemberData(nameof(CoordinateConverterTest.SphericalAndCartesian), MemberType = typeof(CoordinateConverterTest))]
-		public void CoordinateConverter_ConvertFromSphericalToCartesian_ShouldBeExpected(double phi, double theta, double x, double y, double z)
+		public void CoordinateConverter_ConvertFromSphericalToCartesian_BeExpected(double phi, double theta, double x, double y, double z)
 		{
 			// 1. Prepare
 			SphericalVector sc = new SphericalVector(phi, theta);
@@ -43,7 +60,7 @@ namespace Twisty.Engine.Tests.Geometry
 
 		[Theory]
 		[MemberData(nameof(CoordinateConverterTest.SphericalAndCartesian), MemberType = typeof(CoordinateConverterTest))]
-		public void CoordinateConverter_ConvertFromCartesianToSpherical_ShouldBeExpected(double phi, double theta, double x, double y, double z)
+		public void CoordinateConverter_ConvertFromCartesianToSpherical_BeExpected(double phi, double theta, double x, double y, double z)
 		{
 			// 1. Prepare
 			CartesianCoordinate cc = new CartesianCoordinate(x, y, z);
@@ -54,6 +71,40 @@ namespace Twisty.Engine.Tests.Geometry
 			// 3. Verify
 			Assert.Equal(phi, sc.Phi);
 			Assert.Equal(theta, sc.Theta);
+		}
+
+		[Theory]
+		[MemberData(nameof(CoordinateConverterTest.HomogeneousAndCartesian), MemberType = typeof(CoordinateConverterTest))]
+		[MemberData(nameof(CoordinateConverterTest.HomogeneousToCartesianOnly), MemberType = typeof(CoordinateConverterTest))]
+		public void CoordinateConverter_ConvertFromHomogeneousToCartesian_BeExpected(double hx, double hy, double hz, double hw, double x, double y, double z)
+		{
+			// 1. Prepare
+			HomogeneousCoordiante hc = new HomogeneousCoordiante(hx, hy, hz, hw);
+
+			// 2. Execute
+			CartesianCoordinate cc = CoordinateConverter.ConvertToCartesian(hc);
+
+			// 3. Verify
+			Assert.Equal(x, cc.X, PRECISION_DOUBLE);
+			Assert.Equal(y, cc.Y, PRECISION_DOUBLE);
+			Assert.Equal(z, cc.Z, PRECISION_DOUBLE);
+		}
+
+		[Theory]
+		[MemberData(nameof(CoordinateConverterTest.HomogeneousAndCartesian), MemberType = typeof(CoordinateConverterTest))]
+		public void CoordinateConverter_ConvertFromCartesianToHomogeneous_BeExpected(double hx, double hy, double hz, double hw, double x, double y, double z)
+		{
+			// 1. Prepare
+			CartesianCoordinate cc = new CartesianCoordinate(x, y, z);
+
+			// 2. Execute
+			var hc = CoordinateConverter.ConvertToHomogeneous(cc);
+
+			// 3. Verify
+			Assert.Equal(hx, hc.X);
+			Assert.Equal(hy, hc.Y);
+			Assert.Equal(hz, hc.Z);
+			Assert.Equal(hw, hc.W);
 		}
 
 		#endregion Test Methods
--- Twisty.Engine/Geometry/CoordinateConverter.cs
+++ Twisty.Engine/Geometry/CoordinateConverter.cs
@@ -22,17 +22,54 @@ namespace Twisty.Engine.Geometry
 		public static double ConvertRadianToDegree(double radian) => radian * (180 / Math.PI);
 
 		/// <summary>
+		/// Convert a Cartesian Point to his Homogeneous equivalent.
+		/// </summary>
+		/// <param name="cc">Cartesian coordinate to convert.</param>
+		/// <returns>The Vector converted to a Homogeneous representation.</returns>
+		public static HomogeneousCoordiante ConvertToHomogeneous(CartesianCoordinate cc)
+		{
+			return new HomogeneousCoordiante(
+				cc.X,
+				cc.Y,
+				cc.Z,
+				1.0
+			);
+		}
+
+		/// <summary>
 		/// Convert a Spherical Vector to his Cartesian equivalent.
 		/// </summary>
 		/// <param name="sc">Spherical coordinate to convert.</param>
 		/// <returns>The Vector converted to a Cartesian representation.</returns>
 		public static CartesianCoordinate ConvertToCartesian(SphericalVector sc)
 		{
+			double sinTheta = Math.Sin(sc.Theta);
+
 			return new CartesianCoordinate(
-				Math.Sin(sc.Theta) * Math.Cos(sc.Phi),
-				Math.Sin(sc.Theta) * Math.Sin(sc.Phi),
+				sinTheta * Math.Cos(sc.Phi),
+				sinTheta * Math.Sin(sc.Phi),
 				Math.Cos(sc.Theta)
 			);
+		}
+
+		/// <summary>
+		/// Convert a Homogeneous Vector or Point to his Cartesian equivalent.
+		/// </summary>
+		/// <param name="hc">Homogeneous coordinate to convert.</param>
+		/// <returns>The Vector converted to a Cartesian representation.</returns>
+		public static CartesianCoordinate ConvertToCartesian(HomogeneousCoordiante hc)
+		{
+			return hc.W.IsZero()
+				? new CartesianCoordinate(
+					hc.X,
+					hc.Y,
+					hc.Z
+				)
+				: new CartesianCoordinate(
+					hc.X / hc.W,
+					hc.Y / hc.W,
+					hc.Z / hc.W
+				);
 		}
 
 		/// <summary>
@@ -43,7 +80,7 @@ namespace Twisty.Engine.Geometry
 		public static SphericalVector ConvertToSpherical(CartesianCoordinate cc)
 		{
 			// Pre validate value to avoid double.NaN in calculation result.
-			if (cc.IsOnOrigin)
+			if (cc.IsZero)
 				return SphericalVector.Origin;
 
 			return new SphericalVector(
