0ebf0ba9ff26b83a555b495766910912106dd1fe
jardons <sebastien.jardon@moodys.com>
2019-12-04 18:27:15 +0100

    Implement Line/Plane parallelism

--- Twisty.Engine.Tests/Geometry/ParametricLineTest.cs
+++ Twisty.Engine.Tests/Geometry/ParametricLineTest.cs
@@ -5,7 +5,6 @@ namespace Twisty.Engine.Tests.Geometry
 {
 	public class ParametricLineTest
 	{
-
 		private const int PRECISION_DOUBLE = 10;
 
 		#region Test Data
@@ -13,9 +12,20 @@ namespace Twisty.Engine.Tests.Geometry
 		//(CartesianCoordinate p1, CartesianCoordinate p2, double x, double xt, double y, double yt, double z, double zt)
 		public static readonly TheoryData<CartesianCoordinate, CartesianCoordinate, double, double, double, double, double, double> ParametricLinesFromPoints = new TheoryData<CartesianCoordinate, CartesianCoordinate, double, double, double, double, double, double>()
 		{
-			{new CartesianCoordinate(0.0, 0.0, 0.0), new CartesianCoordinate(4.0, 5.0, 6.0), 0.0,4.0,0.0,5.0,0.0,6.0},
-			{new CartesianCoordinate(1.0, 1.0, 1.0), new CartesianCoordinate(4.0, 5.0, 6.0), 1.0,3.0,1.0,4.0,1.0,5.0},
-			{new CartesianCoordinate(1.0, 2.0, 3.0), new CartesianCoordinate(4.0, 5.0, 6.0), 1.0,3.0,2.0,3.0,3.0,3.0},
+			{new CartesianCoordinate(0.0, 0.0, 0.0), new CartesianCoordinate(4.0, 5.0, 6.0), 0.0, 4.0, 0.0, 5.0, 0.0, 6.0},
+			{new CartesianCoordinate(1.0, 1.0, 1.0), new CartesianCoordinate(4.0, 5.0, 6.0), 1.0, 3.0, 1.0, 4.0, 1.0, 5.0},
+			{new CartesianCoordinate(1.0, 2.0, 3.0), new CartesianCoordinate(4.0, 5.0, 6.0), 1.0, 3.0, 2.0, 3.0, 3.0, 3.0},
+		};
+
+		//(string planeCoordinates, string ccCoordinate1, string ccCoordinate2, bool result)
+		public static readonly TheoryData<string, string, string, bool> ParametricLinesAndPlanes = new TheoryData<string, string, string, bool>()
+		{
+			// Parallels lines
+			{ "(1.0 0.0 0.0 20.0)", "(0.0 1.0 1.0)", "(0.0 -1.0 -1.0)", true },
+			{ "(0.0 5.5 0.0 18.9)", "(2.0 0.0 0.0)", "(-2.0 0.0 0.0)", true },
+			// Non-Parallels lines
+			{ "(1.0 0.0 0.0 20.0)", "(0.0 0.0 0.0)", "(4.0 5.0 6.0)", false },
+			{ "(17.34 8.45 13.78 780.9)", "(17.0 0.55 40.301)", "(14.0 -5.0 -76.8)", false },
 		};
 
 		#endregion Test Data
@@ -39,6 +49,23 @@ namespace Twisty.Engine.Tests.Geometry
 			Assert.Equal(yt, o.B, PRECISION_DOUBLE);
 			Assert.Equal(z, o.Z, PRECISION_DOUBLE);
 			Assert.Equal(zt, o.C, PRECISION_DOUBLE);
+		}
+
+		[Theory]
+		[MemberData(nameof(ParametricLineTest.ParametricLinesAndPlanes), MemberType = typeof(ParametricLineTest))]
+		public void ParametricLine_IsParallelToPlane_BeExpected(string planeCoordinates, string ccCoordinate1, string ccCoordinate2, bool result)
+		{
+			// 1. Prepare
+			Plane p = new Plane(planeCoordinates);
+			CartesianCoordinate p1 = new CartesianCoordinate(ccCoordinate1);
+			CartesianCoordinate p2 = new CartesianCoordinate(ccCoordinate2);
+			ParametricLine line = ParametricLine.FromTwoPoints(p1, p2);
+
+			// 2. Execute
+			bool b = line.IsParallelTo(p);
+
+			// 3. Verify
+			Assert.Equal(result, b);
 		}
 
 		#endregion Test Methods
