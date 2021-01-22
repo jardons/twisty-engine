acef43b41a406ab8b795d95ab9c965ba19a49e84
jardons <sebastien.jardon@moodys.com>
2021-01-22 13:11:09 +0100

    Add GeometriException (missing in previous commit)

---
+++ Twisty.Engine/Geometry/GeometricException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twisty.Engine.Geometry
+{
+	/// <summary>
+	/// Exception raised when a Geometric operation is impossible.
+	/// </summary>
+	public class GeometricException : Exception
+	{
+		/// <summary>
+		/// Create a new GeometricException.
+		/// </summary>
+		/// <param name="message">Message of the exception.</param>
+		public GeometricException(string message)
+			: base(message)
+		{
+		}
+	}
+}
