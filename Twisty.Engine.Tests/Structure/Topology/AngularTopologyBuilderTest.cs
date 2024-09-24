03da81f021e242c7ae6cbcbadd5e4c29574c850d
Jardon Sébastien <sebastien.jardon@moodys.com>
2024-09-24 13:36:34 +0200

    Validate ROtated Topologic Id

--- Twisty.Engine.Tests/Structure/Topology/AngularTopologyBuilderTest.cs
+++ Twisty.Engine.Tests/Structure/Topology/AngularTopologyBuilderTest.cs
@@ -64,5 +64,52 @@ public class AngularTopologyBuilderTest
 		Assert.Equal(expected, id);
 	}
 
+	[Theory]
+	// Single Face
+	[InlineData("(1 0 0)", "Y", "(0 0 1)", Math.PI)]
+	[InlineData("(1 0 0)", "Z", "(0 0 1)", Math.PI)]
+	[InlineData("(0 0 1)", "X", "(0 1 1)", Math.PI)]
+	[InlineData("(0 0 1)", "Y", "(0 1 1)", Math.PI)]
+	[InlineData("(0 1 0)", "Y", "(0 0 1)", Math.PI)]
+	[InlineData("(1 1 1)", "Y", "(0 0 1)", Math.PI)]
+	[InlineData("(1 1 1)", "-Y", "(0 0 1)", Math.PI / 3.0)]
+	[InlineData("(-1 -1 -1)", "Y", "(0 0 1)", Math.PI / 2.0)]
+	[InlineData("(-1 -1 -1)", "-Y", "(0 0 1)", Math.PI * 2.0)]
+	// Two Faces
+	[InlineData("(1 0 0)", "Y,Z", "(0 0 1)", Math.PI)]
+	[InlineData("(1 0 0)", "Z,Y", "(0 0 1)", Math.PI)]
+	[InlineData("(1 0 0)", "X,Y", "(0 0 1)", Math.PI)]
+	[InlineData("(1 0 0)", "Y,X", "(0 0 1)", Math.PI)]
+	[InlineData("(1 0 0)", "-X,Y", "(0 0 1)", Math.PI * 0.9)]
+	[InlineData("(1 0 0)", "-Y,X", "(0 0 1)", Math.PI * 5.0)]
+	// Complex
+	[InlineData("(0 0 1)", "X,AlmostX", "(0 0 1)", Math.PI * 5.0)]
+	[InlineData("(0 0 1)", "AlmostX,X", "(0 0 1)", Math.PI * 0.8)]
+	[InlineData("(0 0 1)", "-X,AlmostX", "(0 0 1)", Math.PI * 0.7)]
+	[InlineData("(0 0 1)", "AlmostX,-X", "(0 0 1)", Math.PI * 0.5)]
+	[InlineData("(0 0 1)", "-X,AlmostX,Y", "(8 7 1)", Math.PI * 0.4)]
+	[InlineData("(0 0 1)", "AlmostX,-X,Y", "(13 -4 1)", Math.PI * 0.3)]
+	[InlineData("(0 0 1)", "Y,AlmostX,-X", "(20 100 15)", Math.PI * 0.2)]
+	public void GetRotatedBlockTopologicId_ReturnExpected(string axisCc, string facesId, string rotationAxisCc, double rotationTheta)
+	{
+		// 1. Prepare
+		var axis = new Cartesian3dCoordinate(axisCc);
+		var rotationAxis = new Cartesian3dCoordinate(rotationAxisCc);
+		var b = new Block("id", axis, BlockFacesFactory.GetFaces(facesId));
+		var builder = new AngularTopologyBuilder();
+
+		// Keep original id as rotated one should stay the same.
+		var id = builder.GetTopologicId(b);
+		b.RotateAround(rotationAxis, rotationTheta);
+
+		// 2. Execute
+		var rotatedId = builder.GetTopologicId(b);
+
+		// 3. Verify
+		Assert.NotNull(id);
+		Assert.NotNull(rotatedId);
+		Assert.Equal(id, rotatedId);
+	}
+
 	#endregion Test Methods
 }
