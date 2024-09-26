99035d6da90f541f066e41844696b10e7da149d8
Jardon Sébastien <sebastien.jardon@moodys.com>
2024-09-26 15:30:39 +0200

    Add TopologicContext class

---
+++ Twisty.Engine.Tests/Structure/Topology/TopologicContextTest.cs
@@ -0,0 +1,82 @@
+﻿using Twisty.Engine.Geometry;
+using Twisty.Engine.Structure;
+using Twisty.Engine.Tests.DataFactories;
+using Twisty.Engine.Structure.Topology;
+using Twisty.Engine.Structure.Rubiks;
+
+namespace Twisty.Engine.Tests.Structure;
+
+[Trait("Category", "Structure/Topology")]
+public class TopologicContextTest
+{
+	#region Test Methods
+
+	[Fact]
+	public void CreateEmpty_IsEmpty()
+	{
+		// 1. Prepare
+
+		// 2. Execute
+		var context = new TopologicContext([]);
+
+		// 3. Verify
+		Assert.NotNull(context.TopologicIds);
+		Assert.Empty(context.TopologicIds);
+	}
+
+	[Theory]
+	[InlineData("a", 0)]
+	[InlineData("b", 1)]
+	[InlineData("c", 2)]
+	[InlineData("d", 3)]
+	public void GetTopologicIdIndexInSortedRange_GetIndex(string key, int expected)
+	{
+		// 1. Prepare
+		string[] availableKeys = ["a", "b", "c", "d"];
+		var context = new TopologicContext(availableKeys);
+
+		// 2. Execute
+		int i = context.GetTopologicIdIndex(key);
+
+		// 3. Verify
+		Assert.NotNull(context.TopologicIds);
+		Assert.Equal(expected, i);
+		Assert.Equal(availableKeys.Length, context.TopologicIds.Count);
+	}
+
+	[Theory]
+	[InlineData("a", 0)]
+	[InlineData("b", 1)]
+	[InlineData("c", 2)]
+	[InlineData("d", 3)]
+	public void GetTopologicIdIndexInUnsortedRange_GetIndex(string key, int expected)
+	{
+		// 1. Prepare
+		string[] availableKeys = ["d", "b", "a", "c"];
+		var context = new TopologicContext(availableKeys);
+
+		// 2. Execute
+		int i = context.GetTopologicIdIndex(key);
+
+		// 3. Verify
+		Assert.NotNull(context.TopologicIds);
+		Assert.Equal(expected, i);
+		Assert.Equal(availableKeys.Length, context.TopologicIds.Count);
+	}
+
+	[Fact]
+	public void GetTopologicIdIndexOutOfRange_ThrowsKeyNotFoundException()
+	{
+		// 1. Prepare
+		string[] availableKeys = ["d", "b", "a", "c"];
+		var context = new TopologicContext(availableKeys);
+
+		// 2. Execute
+		Action a = () => context.GetTopologicIdIndex("not there");
+
+		// 3. Verify
+		Assert.Throws<KeyNotFoundException>(a);
+	}
+
+	#endregion Test Methods
+}
---
+++ Twisty.Engine/Structure/Topology/TopologicContext.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Twisty.Engine.Structure.Topology;
+
+/// <summary>
+/// Class describing the creation context information for a TopologyMap.
+/// </summary>
+public class TopologicContext
+{
+	private readonly Dictionary<string, int> m_Index;
+
+	/// <summary>
+	/// Create a new TopologicContext.
+	/// </summary>
+	/// <param name="topologicIds">Collectin of available TopologicId avialable in the context.</param>
+	public TopologicContext(IEnumerable<string> topologicIds)
+	{
+		TopologicIds = topologicIds.OrderBy(id => id).ToList().AsReadOnly();
+		m_Index = [];
+		for (int i = 0; i < TopologicIds.Count; ++i)
+		{
+			m_Index[TopologicIds[i]] = i;
+		}
+	}
+
+	/// <summary>
+	/// Gets the integer index for the provided TopologicId.
+	/// </summary>
+	/// <param name="topologicId">TopologicId search in this Context.</param>
+	/// <returns>The index of the reuqested TopologicId in the TopologicIds table of this Context.</returns>
+	/// <exception cref="KeyNotFoundException">Exception is throwned when the requested TopologicId don't exist in this Context.</exception>
+	public int GetTopologicIdIndex(string topologicId)
+		=> m_Index.TryGetValue(topologicId, out var id)
+			? id
+			: throw new KeyNotFoundException("TopologicId doesn't exist in this context.");
+
+	/// <summary>
+	/// Gets the collection of possible TopologicIds available in this Context.
+	/// </summary>
+	public ReadOnlyCollection<string> TopologicIds { get; }
+}
